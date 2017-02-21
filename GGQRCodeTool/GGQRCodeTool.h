adb4feaa3e3180e9f368e9f17ec61d8131d98523
youngKook <youngKook1992@163.com>
2017-02-21 14:29:45 +0800

    Initial Commit

---
+++ GGQRCodeTool/GGQRCodeTool.h
@@ -0,0 +1,19 @@
+//
+//  GGQRCodeTool.h
+//  GGQRCodeTool
+//
+//  Created by C on 2017/2/21.
+//  Copyright © 2017年 YoungKook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GGQRCodeTool.
+FOUNDATION_EXPORT double GGQRCodeToolVersionNumber;
+
+//! Project version string for GGQRCodeTool.
+FOUNDATION_EXPORT const unsigned char GGQRCodeToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GGQRCodeTool/PublicHeader.h>
+
+

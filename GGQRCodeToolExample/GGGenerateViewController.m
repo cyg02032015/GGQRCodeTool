8f857024e986d1ef1b3de558f526a699042bf17f
youngKook <youngKook1992@163.com>
2018-07-17 13:16:34 +0800

    修改

--- GGQRCodeToolExample/GGGenerateViewController.m
+++ GGQRCodeToolExample/GGGenerateViewController.m
@@ -24,7 +24,7 @@
     [_textField addTarget:self action:@selector(getText:) forControlEvents:UIControlEventEditingChanged];
 }
 - (IBAction)generate:(id)sender {
-    UIImage *img = [GGGenerateTool generrateQRCodeWithContent:_content centerImg:[UIImage imageNamed:@"WechatIMG25.jpeg"]];
+    UIImage *img = [GGGenerateTool generateQRCodeWithContent:_content centerImg:[UIImage imageNamed:@"WechatIMG25.jpeg"]];
     _imgView.image = img;
 }
 - (void)getText:(UITextField *)sender {
